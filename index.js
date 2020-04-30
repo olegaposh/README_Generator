bca71068a8c3520a07253259f86bea1817a2cbc9
olegaposh <oleg.poshiv@gmail.com>
2020-04-29 18:27:03 -0700

    creating new branch

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
+// const questions = [
+
+// ];
+
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
+
+
+
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
 
 const writeFileAsync = util.promisify(fs.writeFile);
+
+
+function promptUser () {
+
+    return inquirer.prompt([
+
+
+
+    ])
+
+
+    .then()
+
+    
+    .catch(err) => {
+        console.log(err)
+    }
+}
